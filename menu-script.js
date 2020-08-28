1e5cad4e2b5a9d4704b9f79ae9eb4129502354a3
Sky <RolyPolie@users.noreply.github.com>
2020-08-28 12:12:11 -0700

    added tab funcionality

--- menu-script.js
+++ menu-script.js
@@ -1,27 +1,31 @@
 // Menu Tabs Content fucntionality for viewing items
 $(document).ready(function () {
-    const activeTab = $("#italianPrep, #hotCoffee, #coldCoffee, #tea")
+    // const italianHide = $("#italianPrep").addClass('is-active');
+    // const hcHide = $("#hotCoffee").addClass('is-active');
+    // const ccHide = $("#coldCoffee").addClass('is-active');
+    // const teaHide = $("#tea").addClass('is-active');
 
     $('#italianPrep').click(function(){
+        $('.hotCoffee_list, .coldCoffee_list').addClass('is-hidden');
+        $('#hotCoffee, #coldCoffee').removeClass('is-active');
         $('.italian_list').removeClass('is-hidden');
+        $('#italianPrep').addClass('is-active');
         
     })
-    $('#hotCoffee').click(function(){
 
+    $('#hotCoffee').click(function(){
+        $('.italian_list, .coldCoffee_list').addClass('is-hidden');
+        $('#coldCoffee, #italianPrep').removeClass('is-active');
         $('.hotCoffee_list').removeClass('is-hidden');
+        $('#hotCoffee').addClass('is-active');
         
     })
     $('#coldCoffee').click(function(){
 
-        // $('#coldCoffee').addClass('is-active');
+        $('.italian_list, .hotCoffee_list').addClass('is-hidden');
+        $('#hotCoffee, #italianPrep').removeClass('is-active');
         $('.coldCoffee_list').removeClass('is-hidden');
+        $('#coldCoffee').addClass('is-active');
         
     })
-    $('#tea').click(function(){
-
-        // $('#tea').addClass('is-active');
-        $('.italian_list').removeClass('is-hidden');
-        
-    })
-
 });
